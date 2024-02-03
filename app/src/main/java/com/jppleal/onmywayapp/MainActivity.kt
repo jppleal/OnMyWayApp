0fe8951770e1693a755210d1e789251d66f41390
José Leal <jppassosleal@gmail.com>
2024-02-03 01:16:13 +0000

    .

--- app/src/main/java/com/jppleal/onmywayapp/MainActivity.kt
+++ app/src/main/java/com/jppleal/onmywayapp/MainActivity.kt
@@ -16,7 +16,7 @@ import com.jppleal.onmywayapp.ui.theme.OnMyWayAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this);
+        FirebaseApp.initializeApp(this)
         setContent {
             OnMyWayAppTheme {
                 // A surface container using the 'background' color from the theme
