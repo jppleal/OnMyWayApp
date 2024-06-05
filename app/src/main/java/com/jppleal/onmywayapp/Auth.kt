cfbb28ff3f3eeb025de5419f431f87f47ceca36f
José Leal <jppassosleal@gmail.com>
2024-06-05 12:24:28 +0100

    Permission for notfications is applied for android version > 13 (TIRAMISU) needs to be check older versions

--- app/src/main/java/com/jppleal/onmywayapp/Auth.kt
+++ app/src/main/java/com/jppleal/onmywayapp/Auth.kt
@@ -29,17 +29,8 @@ class Auth  {
     return null
 }*/
 
-// Function to check if user is logged in
-fun isLoggedIn(context: Context): Boolean {
-    val sharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-    return sharedPreferences.getString("userEmail", null) != null
-}
-
 // Function to save user credentials
-private fun saveUserCredentials(email: String, context: Context) {
-    val sharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-    sharedPreferences.edit().putString("userEmail", email).apply()
-}
+
 
 fun logOut(context: Context) {
     clearUserCredentials(context = context)
--- app/src/main/java/com/jppleal/onmywayapp/MainActivity.kt
+++ app/src/main/java/com/jppleal/onmywayapp/MainActivity.kt
@@ -1,38 +1,60 @@
 package com.jppleal.onmywayapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 import com.jppleal.onmywayapp.ui.theme.OnMyWayAppTheme
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var firebaseRef: DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            firebaseAuth = FirebaseAuth.getInstance()
             NotificationUtils.createNotificationChannel(this)
             FirebaseApp.initializeApp(this);
             OnMyWayAppTheme {
                 /*TODO: Check for permission for notifications*/
-                firebaseRef = FirebaseDatabase.getInstance("https://on-my-way-app-3ixreu-default-rtdb.europe-west1.firebasedatabase.app/").getReference("test")
-                firebaseRef.setValue("Test dude").addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Log.d("Firebase", "Data sent successfully")
-                    } else {
-                        Log.e("Firebase", "Failed to send data: ${task.exception}")
-                    }
+                //Set up your app's theme and content here
+                OnMyWayApp(navController, firebaseAuth.currentUser != null)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    RequestNotificationPermission()
                 }
-                val isLoggedIn = isLoggedIn(this)
-                // Set up your app's theme and content here
-                OnMyWayApp(navController, isLoggedIn)
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    @Composable
+    fun RequestNotificationPermission(){
+        val permissionState = remember { mutableStateOf(false)}
+
+        val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) {
+            isGranted:Boolean ->
+            permissionState.value = isGranted
+        }
+        LaunchedEffect(Unit) {
+            if(ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED)
+                launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+        if(!permissionState.value){
+            /*TODO: Show some UI explanation on why notifications permission is needed*/
         }
     }
 }
